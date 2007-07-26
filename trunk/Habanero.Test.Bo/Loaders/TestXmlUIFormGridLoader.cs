2ef4214bf6f4c304ebdde6846823874fa67ac8d9
Mark Whitfeld <mark.whitfeld@chillisoft.co.za>
2007-07-26 08:05:22 +0000

    Removed test that is dependant on Habanero.UI.Pro

--- trunk/Habanero.Test.Bo/Loaders/TestXmlUIFormGridLoader.cs
+++ trunk/Habanero.Test.Bo/Loaders/TestXmlUIFormGridLoader.cs
@@ -19,15 +19,17 @@ namespace Habanero.Test.Bo.Loaders
             loader = new XmlUIFormGridLoader();
         }
 
-        [Test]
-        public void TestSimpleGrid()
-        {
-            UIFormGrid formGrid =
-                loader.LoadUIFormGrid(
-                    @"<formGrid relationship=""testrelationshipname"" reverseRelationship=""testcorrespondingrelationshipname"" />");
-            Assert.AreEqual("testrelationshipname", formGrid.RelationshipName);
-            Assert.AreEqual("testcorrespondingrelationshipname", formGrid.CorrespondingRelationshipName);
-            Assert.AreEqual(typeof (EditableGrid), formGrid.GridType);
-        }
+		// TODO: This test uses Habanero.UI.Pro, please investigate if this is necessary
+		// and then fix this so that it can be uncommented ( - Mark (2007/07/26) )
+		//[Test]
+		//public void TestSimpleGrid()
+		//{
+		//    UIFormGrid formGrid =
+		//        loader.LoadUIFormGrid(
+		//            @"<formGrid relationship=""testrelationshipname"" reverseRelationship=""testcorrespondingrelationshipname"" />");
+		//    Assert.AreEqual("testrelationshipname", formGrid.RelationshipName);
+		//    Assert.AreEqual("testcorrespondingrelationshipname", formGrid.CorrespondingRelationshipName);
+		//    Assert.AreEqual(typeof (EditableGrid), formGrid.GridType);
+		//}
     }
 }
