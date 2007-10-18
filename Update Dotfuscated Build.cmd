84c83f16de396401af423df4c08b0c5d8849d107
Mark Whitfeld <mark.whitfeld@chillisoft.co.za>
2007-10-18 07:45:06 +0000

    Moved dotfuscation files

--- Update Dotfuscated Build.cmd
+++ Update Dotfuscated Build.cmd
@@ -1,8 +1,8 @@
 set PATH=%PATH%;C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\;C:\Program Files\Microsoft Visual Studio 8\Application\PreEmptive Solutions\Dotfuscator Community Edition\
-sn -Vr Build\Debug\Habanero.UI.Pro.dll
+sn -Vr bin\Habanero.UI.Pro.dll
 Dotfuscator.exe -v Dotfuscator.xml
 sn -Vu Dotfuscated\Habanero.UI.Pro.dll
-sn -R Dotfuscated\Habanero.UI.Pro.dll Habanero.UI.Pro\key.snk
-copy Build\Debug\*.* "Dotfuscated Build\"
-copy Dotfuscated\Habanero.UI.Pro.dll "Dotfuscated Build\"
+sn -R Dotfuscated\Habanero.UI.Pro.dll source\Habanero.UI.Pro\key.snk
+copy bin\*.* "Obfuscated\"
+copy Dotfuscated\Habanero.UI.Pro.dll "Obfuscated\"
 pause
