3f830630a9ac7b6a71c109c660721916050fed5e
Mark Whitfeld <mark.whitfeld@chillisoft.co.za>
2007-11-01 12:51:38 +0000

    Updated Version Number. Updated the PostBuild.cmd file to allow for spaces in the directory names.

--- source/Habanero.Test.Db/Properties/AssemblyInfo.cs
+++ source/Habanero.Test.Db/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chillisoft")]
 [assembly: AssemblyProduct("Habanero.Test.DB")]
-[assembly: AssemblyCopyright("Copyright � Chillisoft 2007")]
+[assembly: AssemblyCopyright("Copyright © Chillisoft 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyFileVersion("1.1.0.*")]
