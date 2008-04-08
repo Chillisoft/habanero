e46a2aa351b6fef5c9ee65750a36a85139de89a2
Mark Whitfeld <mark.whitfeld@chillisoft.co.za>
2008-04-08 15:07:26 +0000

    Removed old licensing folders and files.

--- Update Obfuscated Build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Obfuscating Habanero.UI.Pro.dll....
-"C:\Program Files\Eziriz\.NET Reactor\dotNET_Reactor.exe" -project "licensing\HabaneroPro.nrproj" -q
-if NOT EXIST obfucsated md obfuscated
-copy bin\*.* obfuscated\
-copy licensing\bin\Habanero.UI.Pro.dll Obfuscated\
-copy licensing\bin\Habanero.UI.Pro.pdb Obfuscated\
-pause
