1c6c75ed0faf59fbb12bba54d7d31dbe844906ff
Brett Powell <brett.powell@chillisoft.co.za>
2008-04-18 14:35:49 +0000

    Updated to version 1.3

--- source/Habanero.UI/Properties/AssemblyInfo.cs
+++ source/Habanero.UI/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.*")]
-[assembly: AssemblyFileVersion("1.2.0.*")]
+[assembly: AssemblyVersion("1.3.0.*")]
+[assembly: AssemblyFileVersion("1.3.0.*")]
 
 [assembly: InternalsVisibleTo("Habanero.Test.UI, PublicKeyToken=725f5428c53814ab, PublicKey=00240000048000009400000006020000002400005253413100040000010001004dd499d6d730ef53b7bd17d248b5c8b2fb38d931f369a4e8d3849e6f198ef7a9afdd53c3dd8d65a7c43457cba1b155287ae495bbe2afd64129f42b0f4b7aa815fa57878aec582f96e17c73835e31c3a6fd0b38df8f3f133bbec412f2e63659db477ad3a0c9398080d861285c696feabd04c56817fcfb07d22f4f3e0fa1a43ad8")]
