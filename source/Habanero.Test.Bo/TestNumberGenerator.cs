3d2d2a7af9a04deb19fc1632fad9e0e8cb9dd971
Brett Powell <brett.powell@chillisoft.co.za>
2008-05-05 17:58:30 +0000

    changed tests to clear all loaded bo hope this fixes problem

--- source/Habanero.Test.Bo/TestNumberGenerator.cs
+++ source/Habanero.Test.Bo/TestNumberGenerator.cs
@@ -15,6 +15,7 @@ namespace Habanero.Test.BO
         {
             //Runs every time that any testmethod is executed
             ClassDef.ClassDefs.Clear();
+            BOLoader.Instance.ClearLoadedBusinessObjects();
         }
 
         [TestFixtureSetUp]
