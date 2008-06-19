794c1ed1c1c67b2c58d5d2b6eb6bbc4fbc743464
Eric Savage <eric.savage@chillisoft.co.za>
2008-06-19 14:04:06 +0000

    Changed Habanero version number to v1.5.0

--- source/Common/AssemblyInfoShared.cs
+++ source/Common/AssemblyInfoShared.cs
@@ -23,7 +23,7 @@ using System.Reflection;
 // associated with an assembly.
 [assembly: AssemblyCompany("Chillisoft")]
 [assembly: AssemblyProduct("Habanero")]
-[assembly: AssemblyCopyright("Copyright © Chillisoft 2007")]
+[assembly: AssemblyCopyright("Copyright © Chillisoft 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,5 +36,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.2.0")]
-[assembly: AssemblyFileVersion("1.4.2.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
