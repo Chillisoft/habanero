7f8534d0ad710130a62672c3cce7a0fed85a3175
Soriya Das <soriya.das@chillisoft.co.za>
2008-07-17 14:06:18 +0000

    Added functionality to email error message

--- source/Habanero.UI.WebGUI/FormExceptionNotifier.cs
+++ source/Habanero.UI.WebGUI/FormExceptionNotifier.cs
@@ -19,12 +19,14 @@
 
 #pragma warning disable DoNotCallOverridableMethodsInConstructor
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using Gizmox.WebGUI.Forms;
 using Habanero.Base;
 using Habanero.Base.Exceptions;
 using Habanero.BO;
 using Habanero.UI.Base;
+using Habanero.Util;
 
 namespace Habanero.UI.WebGUI
 {
@@ -58,10 +60,10 @@ namespace Habanero.UI.WebGUI
         /// </summary>
         private class CollapsibleExceptionNotifyForm : FormGiz, IControlChilli
         {
-            private Exception _exception;
-            private PanelGiz _summary;
-            private PanelGiz _fullDetail;
-            private IButton _moreDetailButton;
+            private readonly Exception _exception;
+            private readonly PanelGiz _summaryPanel;
+            private PanelGiz _fullDetailPanel;
+            private readonly IButton _moreDetailButton;
             private TextBoxGiz _errorDetails;
             private CheckBoxGiz _showStackTrace;
             private const int SUMMARY_HEIGHT = 150;
@@ -75,30 +77,32 @@ namespace Habanero.UI.WebGUI
             {
                 _exception = ex;
 
-                _summary = new PanelGiz();
-                _summary.Text = title;
-                _summary.Height = SUMMARY_HEIGHT;
+                _summaryPanel = new PanelGiz();
+                _summaryPanel.Text = title;
+                _summaryPanel.Height = SUMMARY_HEIGHT;
                 ITextBox messageTextBox = GetSimpleMessage(ex.Message);
                 ILabel messageLabel = GetErrorLabel(furtherMessage);
-                BorderLayoutManager summaryManager = new BorderLayoutManagerGiz(_summary, GlobalUIRegistry.ControlFactory);
+                BorderLayoutManager summaryManager = new BorderLayoutManagerGiz(_summaryPanel, GlobalUIRegistry.ControlFactory);
                 summaryManager.AddControl(messageLabel, BorderLayoutManager.Position.North);
                 summaryManager.AddControl(messageTextBox, BorderLayoutManager.Position.Centre);
 
                 IButtonGroupControl buttonsOK = GlobalUIRegistry.ControlFactory.CreateButtonGroupControl();
-                IButtonGroupControl buttonsDetail = GlobalUIRegistry.ControlFactory.CreateButtonGroupControl();
                 buttonsOK.AddButton("&OK", OKButtonClickHandler);
-                _moreDetailButton = buttonsDetail.AddButton("More Detail...", MoreDetailClickHandler);
                 buttonsOK.Height = BUTTONS_HEIGHT;
+
+                IButtonGroupControl buttonsDetail = GlobalUIRegistry.ControlFactory.CreateButtonGroupControl();
+                buttonsDetail.AddButton("Email Error", EmailErrorClickHandler);
+                _moreDetailButton = buttonsDetail.AddButton("More Detail...", MoreDetailClickHandler);
                 buttonsDetail.Height = BUTTONS_HEIGHT;
-                buttonsDetail.Width = _moreDetailButton.Width + 9;
+                buttonsDetail.Width = 2 * (_moreDetailButton.Width + 9);
 
                 SetFullDetailsPanel();
 
                 BorderLayoutManager manager = new BorderLayoutManagerGiz(this, GlobalUIRegistry.ControlFactory);
-                manager.AddControl(_summary, BorderLayoutManager.Position.North);
+                manager.AddControl(_summaryPanel, BorderLayoutManager.Position.North);
                 manager.AddControl(buttonsDetail, BorderLayoutManager.Position.West);
                 manager.AddControl(buttonsOK, BorderLayoutManager.Position.East);
-                manager.AddControl(_fullDetail, BorderLayoutManager.Position.South);
+                manager.AddControl(_fullDetailPanel, BorderLayoutManager.Position.South);
 
 
                 Text = title;
@@ -107,6 +111,50 @@ namespace Habanero.UI.WebGUI
                 StartPosition = FormStartPosition.Manual;
                 Location = new Point(50, 50);
                 Resize += ResizeForm;
+            }
+
+            private void EmailErrorClickHandler(object sender, EventArgs e)
+            {
+                try
+                {
+                    
+                string emailTo = GlobalRegistry.Settings.GetString("EMAIL_TO");
+                List<string> emailAddresses = new List<string>(1);
+                emailAddresses.Add(emailTo);
+
+                string emailFrom = GlobalRegistry.Settings.GetString("EMAIL_FROM");
+
+                string emailContent = ExceptionUtilities.GetExceptionString(_exception, 0, true);
+
+                EmailSender emailSender = new EmailSender(emailAddresses, emailFrom, _exception.Source, emailContent, "");
+                //Todo : check Send Authenticated for security purposes?
+                emailSender.SmtpServerHost = GlobalRegistry.Settings.GetString("SMTP_SERVER");
+                string port = GlobalRegistry.Settings.GetString("SMTP_SERVER_PORT");
+                if (!String.IsNullOrEmpty(port))
+                {
+                    emailSender.SmtpServerPort = Convert.ToInt32(port);
+                }
+                bool enableSSL = GlobalRegistry.Settings.GetBoolean("SMTP_ENABLE_SSL");
+                emailSender.EnableSSL = enableSSL;
+                //string authUsername = GlobalRegistry.Settings.GetString("SMTP_AUTH_USERNAME");
+                //string authPassword = GlobalRegistry.Settings.GetString("SMTP_AUTH_PASSWORD");
+                //if (!String.IsNullOrEmpty(authPassword))
+                //{
+                //    authPassword = Encryption.Decrypt(authPassword);
+                //}
+                //if (!String.IsNullOrEmpty(authUsername))
+                //{
+                //    emailSender.SendAuthenticated(authUsername, authPassword);
+                //}
+                //else
+                //{
+                emailSender.Send();
+                //}
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("The error message was not sent due to the following error : " + Environment.NewLine + ex.Message);
+                }
             }
 
             /// <summary>
@@ -142,10 +190,10 @@ namespace Habanero.UI.WebGUI
             /// </summary>
             private void SetFullDetailsPanel()
             {
-                _fullDetail = new PanelGiz();
-                _fullDetail.Text = "Error Detail";
-                _fullDetail.Height = FULL_DETAIL_HEIGHT;
-                _fullDetail.Visible = false;
+                _fullDetailPanel = new PanelGiz();
+                _fullDetailPanel.Text = "Error Detail";
+                _fullDetailPanel.Height = FULL_DETAIL_HEIGHT;
+                _fullDetailPanel.Visible = false;
                 _errorDetails = new TextBoxGiz();
                 _errorDetails.Text = ExceptionUtilities.GetExceptionString(_exception, 0, false);
                 _errorDetails.Multiline = true;
@@ -153,7 +201,7 @@ namespace Habanero.UI.WebGUI
                 _showStackTrace = new CheckBoxGiz();
                 _showStackTrace.Text = "&Show stack trace";
                 _showStackTrace.CheckedChanged += ShowStackTraceClicked;
-                BorderLayoutManager detailsManager = new BorderLayoutManagerGiz(_fullDetail, GlobalUIRegistry.ControlFactory);
+                BorderLayoutManager detailsManager = new BorderLayoutManagerGiz(_fullDetailPanel, GlobalUIRegistry.ControlFactory);
                 detailsManager.AddControl(_errorDetails, BorderLayoutManager.Position.Centre);
                 detailsManager.AddControl(_showStackTrace, BorderLayoutManager.Position.South);
             }
@@ -172,17 +220,17 @@ namespace Habanero.UI.WebGUI
             /// </summary>
             private void MoreDetailClickHandler(object sender, EventArgs e)
             {
-                if (!_fullDetail.Visible)
+                if (!_fullDetailPanel.Visible)
                 {
-                    Height = _summary.Height + BUTTONS_HEIGHT + 16 + FULL_DETAIL_HEIGHT;
+                    Height = _summaryPanel.Height + BUTTONS_HEIGHT + 16 + FULL_DETAIL_HEIGHT;
                     Width = 750;
-                    _fullDetail.Visible = true;
+                    _fullDetailPanel.Visible = true;
                     _moreDetailButton.Text = "� &Less Detail";
                 }
                 else
                 {
-                    Height = _summary.Height + BUTTONS_HEIGHT + 16;
-                    _fullDetail.Visible = false;
+                    Height = _summaryPanel.Height + BUTTONS_HEIGHT + 16;
+                    _fullDetailPanel.Visible = false;
                     _moreDetailButton.Text = "&More Detail �";
                 }
             }
@@ -205,15 +253,15 @@ namespace Habanero.UI.WebGUI
                 {
                     sdHeight = SUMMARY_HEIGHT;
                 }
-                _summary.Height = sdHeight;
+                _summaryPanel.Height = sdHeight;
                 int heightRemaining = Height - BUTTONS_HEIGHT - sdHeight - 16;
                 if (heightRemaining > 0)
                 {
-                    _fullDetail.Height = heightRemaining;
+                    _fullDetailPanel.Height = heightRemaining;
                 }
                 else
                 {
-                    _fullDetail.Height = 0;
+                    _fullDetailPanel.Height = 0;
                 }
             }
 
