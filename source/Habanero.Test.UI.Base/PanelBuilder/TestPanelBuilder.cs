db3b7b8f2be552330fa37a2c7c33ad698fc6ad72
Hagashen Naidu <hagashen.naidu@chillisoft.co.za>
2008-11-04 15:38:11 +0000

    Added layout code to the PanelBuilder

--- source/Habanero.Test.UI.Base/PanelBuilder/TestPanelBuilder.cs
+++ source/Habanero.Test.UI.Base/PanelBuilder/TestPanelBuilder.cs
@@ -21,7 +21,6 @@ namespace Habanero.Test.UI.Base.PanelBuilder
         public void TestConstructor()
         {
             //---------------Set up test pack-------------------
-            Sample.SampleUserInterfaceMapperWin sampleUserInterfaceMapperWin = new Sample.SampleUserInterfaceMapperWin();
             //---------------Assert Precondition----------------
 
             //---------------Execute Test ----------------------
@@ -158,7 +157,6 @@ namespace Habanero.Test.UI.Base.PanelBuilder
             IPanel panel = panelBuilder.BuildPanel(simpleUIDefTwoColumns);
             //---------------Test Result -----------------------
             Assert.AreEqual(6,panel.Controls.Count);
-
         }
 
         [Test]
@@ -173,10 +171,70 @@ namespace Habanero.Test.UI.Base.PanelBuilder
             //---------------Execute Test ----------------------
             IPanel panel = panelBuilder.BuildPanel(simpleUIDefTwoColumns);
             //---------------Test Result -----------------------
-            Assert.AreEqual(9, panel.Controls.Count);
-
+            Assert.AreEqual(12, panel.Controls.Count);
         }
 
+        [Test]
+        public void Test_TwoColumns_TwoRows_DifferentNumberOfControls_Layout()
+        {
+            //---------------Set up test pack-------------------
+            Sample.SampleUserInterfaceMapperWin sampleUserInterfaceMapperWin = new Sample.SampleUserInterfaceMapperWin();
+            UIForm simpleUIDefTwoColumns = sampleUserInterfaceMapperWin.GetSimpleUIFormDef2Row2Columns1RowWithMoreControls();
+            PanelBuilder panelBuilder = new PanelBuilder(GetControlFactory());
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            IPanel panel = panelBuilder.BuildPanel(simpleUIDefTwoColumns);
+            //---------------Test Result -----------------------
+            IControlHabanero control = panel.Controls[0];
+            Assert.AreEqual(5, control.Left);
+          
+
+            IControlHabanero control1 = panel.Controls[1];
+            int gap = 2;
+            Assert.AreEqual(control.Right + gap, control1.Left);
+    
+
+            IControlHabanero control2 = panel.Controls[2];
+            Assert.AreEqual(control1.Right + gap, control2.Left);
+          
+
+            IControlHabanero control3 = panel.Controls[3];
+            Assert.IsInstanceOfType(typeof(IControlHabanero), control3);
+      
+
+            IControlHabanero control4 = panel.Controls[4];
+            Assert.IsInstanceOfType(typeof(IControlHabanero), control4);
+       
+
+            IControlHabanero control5 = panel.Controls[5];
+            Assert.IsInstanceOfType(typeof(IControlHabanero), control5);
+   
+
+            IControlHabanero control6 = panel.Controls[6];
+            Assert.AreEqual(5, control6.Left);
+          
+
+            IControlHabanero control7 = panel.Controls[7];
+            Assert.AreEqual(control6.Right + gap, control7.Left);
+       
+
+            IControlHabanero control8 = panel.Controls[8];
+            Assert.AreEqual(control7.Right + gap, control8.Left);
+
+            IControlHabanero control9 = panel.Controls[9];
+            Assert.AreEqual(control8.Right + gap, control9.Left);
+
+
+            IControlHabanero control10 = panel.Controls[10];
+            Assert.AreEqual(control9.Right + gap, control10.Left);
+
+
+            IControlHabanero control11 = panel.Controls[11];
+            Assert.AreEqual(control10.Right + gap, control11.Left);
+        
+
+        }
     }
 
 
@@ -203,22 +261,56 @@ namespace Habanero.Test.UI.Base.PanelBuilder
             GridLayoutManager layoutManager = new GridLayoutManager(panel,Factory);
             int rowCount=0;
             rowCount = GetRowCount(form, rowCount);
-            layoutManager.SetGridSize(rowCount, form[0].Count * 3);
-            foreach (UIFormTab formTab in form)
+            int columns = form[0].Count;
+            int cols = rowCount * 3;
+            GridLayoutManager.ControlInfo[,] controls =
+                new GridLayoutManager.ControlInfo[columns, cols];
+
+            layoutManager.SetGridSize(columns, cols);
+            GetControls(form, controls);
+            AddControlsToLayoutManager(columns, cols, layoutManager, controls);
+            return panel;
+        }
+
+        private void AddControlsToLayoutManager(int rowCount, int cols, GridLayoutManager layoutManager, GridLayoutManager.ControlInfo[,] controls)
+        {
+            for (int i = 0; i < rowCount; i++)
             {
-                foreach (UIFormColumn formColumn in formTab)
+                for (int j = 0; j < cols; j++)
                 {
-                    foreach (UIFormField formField in formColumn)
+                    if (controls[i, j] == null)
                     {
-                        ILabel label = Factory.CreateLabel(formField.GetLabel());
-                        layoutManager.AddControl(label);
-                        IControlHabanero controlHabanero = Factory.CreateControl(formField.ControlTypeName, formField.ControlAssemblyName);
-                        layoutManager.AddControl(controlHabanero);
-                        layoutManager.AddControl(Factory.CreatePanel());
+                        layoutManager.AddControl(null);
+                    }
+                    else
+                    {
+                        layoutManager.AddControl(controls[i, j].Control, controls[i, j].ColumnSpan, controls[i, j].RowSpan);
+                        controls[i, j].Control.TabIndex = rowCount * j + i;
                     }
                 }
             }
-            return panel;
+        }
+
+        private void GetControls(UIForm form, GridLayoutManager.ControlInfo[,] controls)
+        {
+            foreach (UIFormTab formTab in form)
+            {
+                int currentRow = 0;
+                foreach (UIFormColumn formColumn in formTab)
+                {
+                    int currentColumn = 0;
+                    foreach (UIFormField formField in formColumn)
+                    {
+                        ILabel label = Factory.CreateLabel(formField.GetLabel());
+                        controls[currentRow, currentColumn + 0] = new GridLayoutManager.ControlInfo(label);
+                        IControlHabanero controlHabanero = Factory.CreateControl(formField.ControlTypeName, formField.ControlAssemblyName);
+                        controls[currentRow, currentColumn + 1] = new GridLayoutManager.ControlInfo(controlHabanero);
+                        controls[currentRow, currentColumn + 2] = new GridLayoutManager.ControlInfo(Factory.CreatePanel());
+                        currentColumn += 3;
+                    }
+                    currentRow++;
+                }
+            }
         }
 
         private int GetRowCount(UIForm form, int rowCount)
