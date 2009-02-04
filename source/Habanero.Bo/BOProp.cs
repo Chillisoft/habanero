1af56635f773d5d2bbd61c1c5cf41c874883060c
Brett Powell <brett.powell@chillisoft.co.za>
2009-02-04 10:59:30 +0000

    Fixed bug where a property is set to a new value and reset to the original value it was still Dirty

--- source/Habanero.Bo/BOProp.cs
+++ source/Habanero.Bo/BOProp.cs
@@ -291,7 +291,7 @@ namespace Habanero.BO
                 _valueBeforeLastEdit = _currentValue;
                 _currentValue = newValue;
                 FireBOPropValueUpdated();
-                _isDirty = true;
+                _isDirty = !Equals(_persistedValue, newValue);
             }
         }
 
