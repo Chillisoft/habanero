205776118bf1dc2d0e874788ad80c2f5bb2243e1
Deerasha Singh <deerasha.singh@chillisoft.co.za>
2009-04-15 07:57:06 +0000

    Added a todo in IFormHabanero. Closing event needs to be implemented in FormWin and FormVWG

--- source/Habanero.UI.Base/ControlInterfaces/IFormHabanero.cs
+++ source/Habanero.UI.Base/ControlInterfaces/IFormHabanero.cs
@@ -154,6 +154,12 @@ namespace Habanero.UI.Base
         /// </summary>
         event EventHandler Closed;
 
+        //TODO Deerasha 15Apr2009 implement this in FormWin and FormVWG
+        /// <summary>
+        /// Occurs before closing the form
+        /// </summary>
+        //event EventHandler Closing;
+        
         /// <summary>
         /// Shows the form as a modal dialog box with the currently active window set as its owner
         /// </summary>
