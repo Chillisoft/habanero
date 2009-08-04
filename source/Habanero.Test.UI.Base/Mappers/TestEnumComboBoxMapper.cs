5a980916ec1762028e2dc7def07ea08b326f1169
Eric Savage <eric.savage@chillisoft.co.za>
2009-08-04 16:06:29 +0000

    Started work on the EnumComboBoxMapper, which will be used in Firestarter.

---
+++ source/Habanero.Test.UI.Base/Mappers/TestEnumComboBoxMapper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Habanero.Base;
+using Habanero.Base.Exceptions;
+using Habanero.BO;
+using Habanero.BO.ClassDefinition;
+using Habanero.UI.Base;
+using Habanero.UI.Win;
+using NUnit.Framework;
+
+namespace Habanero.Test.UI.Base.Mappers
+{
+    [TestFixture]
+    public class TestEnumComboBoxMapper
+    {
+        private const string ENUM_PROP_NAME = "EnumProp";
+        private const string ENUM_PKPROP_NAME = "EnumPropPK";
+
+        [SetUp]
+        public void Setup()
+        {
+            ClassDef.ClassDefs.Clear();
+            ClassDef.ClassDefs.Add(GetClassDef());
+        }
+
+        [Test]
+        public void Test_PopulatesComboBoxWithEnum()
+        {
+            //---------------Set up test pack-------------------
+            EnumBO bo = new EnumBO();
+            ComboBoxWin comboBox = new ComboBoxWin();
+            IControlFactory controlFactory = new ControlFactoryWin();
+            EnumComboBoxMapper enumComboBoxMapper = new EnumComboBoxMapper(comboBox, ENUM_PROP_NAME, false, controlFactory);
+            enumComboBoxMapper.BusinessObject = bo;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            enumComboBoxMapper.SetupComboBoxItems();
+            //---------------Test Result -----------------------
+            Assert.AreEqual(3, comboBox.Items.Count);
+            Assert.AreEqual(TestEnum.Option1.ToString(), comboBox.Items[0].ToString());
+            Assert.AreEqual(TestEnum.Option2.ToString(), comboBox.Items[1].ToString());
+            Assert.AreEqual(TestEnum.Option3.ToString(), comboBox.Items[2].ToString());
+        }
+
+        [Test]
+        public void Test_ExceptionThrownIfPropertyTypeNotEnum()
+        {
+            //---------------Set up test pack-------------------
+            EnumBO bo = new EnumBO();
+            ComboBoxWin comboBox = new ComboBoxWin();
+            IControlFactory controlFactory = new ControlFactoryWin();
+            EnumComboBoxMapper enumComboBoxMapper = new EnumComboBoxMapper(comboBox, ENUM_PKPROP_NAME, false, controlFactory);
+            enumComboBoxMapper.BusinessObject = bo;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            try
+            {
+                enumComboBoxMapper.SetupComboBoxItems();
+                //---------------Test Result -----------------------
+                Assert.Fail("Expected to throw an InvalidPropertyException");
+            } 
+            catch (InvalidPropertyException ex)
+            {
+                StringAssert.Contains("EnumComboBoxMapper can only be used for an enum property type", ex.Message);
+            }
+        }
+
+        //TODO Eric 04 Aug 2009: implement other methods, add tests for catch scenarios/boundary conditions
+
+        private static ClassDef GetClassDef()
+        {
+            PropDef propDefPK = new PropDef(ENUM_PKPROP_NAME, typeof(Guid), PropReadWriteRule.WriteNew, null);
+            PropDef propDef = new PropDef(ENUM_PROP_NAME, typeof(TestEnum), PropReadWriteRule.ReadWrite, TestEnum.Option1);
+            PrimaryKeyDef primaryKeyDef = new PrimaryKeyDef { propDefPK };
+            PropDefCol propDefCol = new PropDefCol { propDefPK, propDef };
+
+            UIFormField uiFormField = new UIFormField(TestUtil.GetRandomString(), propDef.PropertyName,
+                typeof(ComboBox), "EnumComboBoxMapper", "Habanero.UI.Base", true, null, null, LayoutStyle.Label);
+            UIFormColumn uiFormColumn = new UIFormColumn { uiFormField };
+            UIFormTab uiFormTab = new UIFormTab { uiFormColumn };
+            UIForm uiForm = new UIForm { uiFormTab };
+            UIDef uiDef = new UIDef("default", uiForm, null);
+            UIDefCol uiDefCol = new UIDefCol { uiDef };
+
+            ClassDef classDef = new ClassDef(typeof(EnumBO), primaryKeyDef, propDefCol, new KeyDefCol(), null, uiDefCol);
+            return classDef;
+        }
+
+        public enum TestEnum
+        {
+            Option1,
+            Option2,
+            Option3
+        }
+
+        public class EnumBO : BusinessObject
+        {
+            
+        }
+    }
+}
---
+++ source/Habanero.UI.Base/ControlMappers/EnumComboBoxMapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Habanero.Base;
+using Habanero.Base.Exceptions;
+
+namespace Habanero.UI.Base
+{
+    /// <summary>
+    /// TODO: comment 
+    /// </summary>
+    public class EnumComboBoxMapper : ComboBoxMapper
+    {
+        public EnumComboBoxMapper(IComboBox comboBox, string propName, bool isReadOnly, IControlFactory factory) : base(comboBox, propName, isReadOnly, factory)
+        {
+        }
+
+        public override void ApplyChangesToBusinessObject()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InternalUpdateControlValueFromBo()
+        {
+            //
+        }
+
+        protected internal override void SetupComboBoxItems()
+        {
+            IPropDef propDef = _businessObject.ClassDef.PropDefcol[_propertyName];
+            if (!propDef.PropertyType.IsEnum)
+            {
+                throw new InvalidPropertyException("EnumComboBoxMapper can only be used for an enum property type");
+            }
+
+            string[] names = Enum.GetNames(propDef.PropertyType);
+            foreach (string name in names)
+            {
+                _comboBox.Items.Add(name);
+            }
+        }
+    }
+}
