6d6f23099d22af3716258ab18f10766a48d448ad
Mark Whitfeld <mark.whitfeld@chillisoft.co.za>
2009-09-01 13:20:33 +0000

    Moved CrossThreadTestRunner to Habanero.Base(Util) and renamed it to CrossThreadRunner.

--- source/Habanero.Base/Util/CrossThreadRunner.cs
+++ source/Habanero.Base/Util/CrossThreadRunner.cs
@@ -1,27 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Security.Permissions;
-using System.Text;
 using System.Threading;
 
-namespace Habanero.Testing.Base
+namespace Habanero.Util
 {
     /// <summary>
-    /// This class is used when you specifically require a single test method to run in MTA or STA thread.
+    /// This class is used when you specifically require some code method to run in MTA or STA thread.
+    /// </summary>
+    /// <remarks>
     /// Unfortunately not all test runners implement the [RequireSTA] or other attributes such as config files.
     /// This may result in tests passing in NUnit GUI failing on the build server or failing on another developer's machine
     /// e.g. someone using Resharper. By using this Component you can ensure that the Individual test runs as required.
     /// The common requirement for STA threading is a visual component under test.
+    /// </remarks>
     /// <example>
     /// <code>
     ///        [Test]<br/>
     ///        public void Test_SetSolution_WithNull_ShouldRaiseError()<br/>
     ///        {<br/>
-    ///            CrossThreadTestRunner runner = new CrossThreadTestRunner();<br/>
-    ///            runner.RunInSTA(<br/>
-    ///                delegate<br/>
+    ///            CrossThreadRunner runner = new CrossThreadRunner();<br/>
+    ///            runner.RunInSTA(delegate<br/>
     ///                    {<br/>
     ///                        //---------------Set up test pack-------------------<br/>
     ///                        ReportSourceDefCreatorControl creatorControl = new ReportSourceDefCreatorControl();<br/>
@@ -40,17 +39,25 @@ namespace Habanero.Testing.Base
     ///                        }<br/>
     ///                    });<br/>
     ///        }<br/>
-    /// </code></example> 
-    /// </summary>
-    public class CrossThreadTestRunner
+    /// </code>
+    /// </example> 
+    public class CrossThreadRunner
     {
         private Exception _lastException;
 
+        ///<summary>
+        /// Run the specified delegate in the MTA Thread.
+        ///</summary>
+        ///<param name="userDelegate">The delegate to be run in the MTA Thread</param>
         public void RunInMTA(ThreadStart userDelegate)
         {
             Run(userDelegate, ApartmentState.MTA);
         }
 
+        ///<summary>
+        /// Run the specified delegate in the STA Thread.
+        ///</summary>
+        ///<param name="userDelegate">The delegate to be run in the STA Thread</param>
         public void RunInSTA(ThreadStart userDelegate)
         {
             Run(userDelegate, ApartmentState.STA);
