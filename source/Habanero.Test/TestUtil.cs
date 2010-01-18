df7010a0490dd35400cc1348b3625800c8f09133
Donald Mafa <donald.mafa@chillisoft.co.za>
2010-01-18 17:11:13 +0000

    Updated new branch 2.3.2 with correct database backups

--- source/Habanero.Test/TestUtil.cs
+++ source/Habanero.Test/TestUtil.cs
@@ -190,7 +190,7 @@ namespace Habanero.Test
         }
 
         public static DatabaseConfig GetDatabaseConfig() {
-            return new DatabaseConfig("MySql", "localhost", "habanero_test_trunk", "root", "root", "3306");
+            return new DatabaseConfig("MySql", "localhost", "habanero_test_branch_2_3_2", "root", "root", "3306");
         }
 
         public static bool GetRandomBoolean()
