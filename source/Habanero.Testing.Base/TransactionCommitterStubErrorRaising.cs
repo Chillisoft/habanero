41fa638a7031d4227843306c8e58a09da1b02ebb
Brett Powell <brett.powell@chillisoft.co.za>
2010-01-19 16:10:37 +0000

    Added TransactionCommitter that raises error instead of Committing to datasource. This can then be used for testing e.g. when a wizard needs to undo a number of things if the Persist fails.

---
+++ source/Habanero.Testing.Base/TransactionCommitterStubErrorRaising.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Habanero.Base;
+using Habanero.BO;
+
+namespace Habanero.Testing.Base
+{
+    public class TransactionCommitterInMemoryStubErrorRaising : TransactionCommitterInMemory
+    {
+        public TransactionCommitterInMemoryStubErrorRaising(DataStoreInMemory dataStoreInMemory) : base(dataStoreInMemory)
+        {
+
+        }
+
+        protected override bool CommitToDatasource()
+        {
+            throw new Exception("");
+        }
+    }
+}
