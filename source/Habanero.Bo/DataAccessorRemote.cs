e7b3f52bb401c1fb3c97f742fa99de2d94840242
Peter Wiles <peter.wiles@chillisoft.co.za>
2010-01-22 12:41:12 +0000

    Added the DataAccessorRemote class which is a very simple wrapper for a remote IDataAccessor.

---
+++ source/Habanero.Bo/DataAccessorRemote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Habanero.Base;
+using Habanero.BO;
+
+namespace Habanero.BO
+{
+    public class DataAccessorRemote : IDataAccessor
+    {
+        private readonly IDataAccessor _remoteDataAccessor;
+
+        public DataAccessorRemote(IDataAccessor remoteDataAccessor) {
+            _remoteDataAccessor = remoteDataAccessor;
+        }
+
+        public ITransactionCommitter CreateTransactionCommitter() {
+            return new TransactionCommitterRemote( _remoteDataAccessor.CreateTransactionCommitter());
+        }
+        public IBusinessObjectLoader BusinessObjectLoader
+        {
+            get { return _remoteDataAccessor.BusinessObjectLoader; }
+        }
+
+    }
+   
+}
