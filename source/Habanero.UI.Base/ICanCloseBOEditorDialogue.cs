27952cf527c2a68b1e8cb51182c0b3f9822cb359
Peter Wiles <peter.wiles@chillisoft.co.za>
2010-03-31 15:50:33 +0000

    Moved UI stuff to Faces folder

--- source/Habanero.UI.Base/ICanCloseBOEditorDialogue.cs
+++
@@ -1,9 +0,0 @@
-namespace Habanero.UI.Base
-{
-    /// <summary>
-    /// Represents a panel that contains an OK and Cancel button
-    /// </summary>
-    public interface ICanCloseBOEditorDialogue: IFormHabanero
-    {
-    }
-}
