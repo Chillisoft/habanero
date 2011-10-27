548f27d800bf27bc7e87b0b3a6037e355e535f79
Soriya Das <soriya.das@chillisoft.co.za>
2011-10-27 12:59:36 +0000

    Added license info to all files.

--- source/Habanero.Base/Util/CrossThreadRunner.cs
+++ source/Habanero.Base/Util/CrossThreadRunner.cs
@@ -1,21 +1,21 @@
-// ---------------------------------------------------------------------------------
-//  Copyright (C) 2007-2010 Chillisoft Solutions
-//  
-//  This file is part of the Habanero framework.
-//  
-//      Habanero is a free framework: you can redistribute it and/or modify
-//      it under the terms of the GNU Lesser General Public License as published by
-//      the Free Software Foundation, either version 3 of the License, or
-//      (at your option) any later version.
-//  
-//      The Habanero framework is distributed in the hope that it will be useful,
-//      but WITHOUT ANY WARRANTY; without even the implied warranty of
-//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//      GNU Lesser General Public License for more details.
-//  
-//      You should have received a copy of the GNU Lesser General Public License
-//      along with the Habanero framework.  If not, see <http://www.gnu.org/licenses/>.
-// ---------------------------------------------------------------------------------
+//  ---------------------------------------------------------------------------------
+//   Copyright (C) 2007-2010 Chillisoft Solutions
+//   
+//   This file is part of the Habanero framework.
+//   
+//       Habanero is a free framework: you can redistribute it and/or modify
+//       it under the terms of the GNU Lesser General Public License as published by
+//       the Free Software Foundation, either version 3 of the License, or
+//       (at your option) any later version.
+//   
+//       The Habanero framework is distributed in the hope that it will be useful,
+//       but WITHOUT ANY WARRANTY; without even the implied warranty of
+//       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//       GNU Lesser General Public License for more details.
+//   
+//       You should have received a copy of the GNU Lesser General Public License
+//       along with the Habanero framework.  If not, see <http://www.gnu.org/licenses/>.
+//  ---------------------------------------------------------------------------------
 using System;
 using System.Reflection;
 using System.Security.Permissions;
