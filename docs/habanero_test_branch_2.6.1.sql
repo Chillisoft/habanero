3e122c462ce16882dd576f90a0fefdcc1bb0017f
Soriya Das <soriya.das@chillisoft.co.za>
2012-02-13 08:32:28 +0000

    Updated version number to v2.6.1

--- docs/habanero_test_branch_2.6.1.sql
+++ docs/habanero_test_branch_2.6.1.sql
@@ -15,11 +15,11 @@
 
 
 --
--- Create schema habanero_test_trunk
+-- Create schema habanero_test_branch_2_6_1
 --
 
-CREATE DATABASE IF NOT EXISTS habanero_test_trunk;
-USE habanero_test_trunk;
+CREATE DATABASE IF NOT EXISTS habanero_test_branch_2_6_1;
+USE habanero_test_branch_2_6_1;
 
 --
 -- Definition of table `another_number_generator`
--- source/Habanero.Test/MyDBConnection.cs
+++ source/Habanero.Test/MyDBConnection.cs
@@ -40,7 +40,7 @@ namespace Habanero.Test
 
             settings.Add("vendor", DatabaseConfig.MySql);
             settings.Add("server", "localhost");
-            settings.Add("database", "habanero_test_trunk");
+            settings.Add("database", "habanero_test_branch_2_6_1");
             settings.Add("username", "root");
             settings.Add("password", "root");
             settings.Add("port", "3306");
