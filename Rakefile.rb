56a033cfd0001ae679295a3053f453c08fa4e9a1
Barry Dwyer <barry.dwyer@chillisoft.co.za>
2012-04-24 13:01:34 +0000

    Updated Rakefile.rb to publish to nuget

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,61 +1,89 @@
 require 'rake'
 require 'albacore'
-    
-task :default => [:build_habanero] #this is the starting point for the script
 
-task :build_habanero => [:clean_habanero,:msbuild_habanero,:run_nunit] # a list of tasks spawned off the default task
+#______________________________________________________________________________
+#---------------------------------SETTINGS-------------------------------------
 
-$Nunit_path = "C:/Program Files (x86)/NUnit 2.5.6/bin/net-2.0/nunit-console-x86.exe"
+# set up the build script folder so we can pull in shared rake scripts.
+# This should be the same for most projects, but if your project is a level
+# deeper in the repo you will need to add another ..
+bs = File.dirname(__FILE__)
+bs = File.join(bs, "..") if bs.index("branches") != nil
+bs = File.join(bs, "../../HabaneroCommunity/BuildScripts")
+$buildscriptpath = File.expand_path(bs)
+$:.unshift($buildscriptpath) unless
+    $:.include?(bs) || $:.include?($buildscriptpath)
 
-#build_habanero tasks
-task :clean_habanero do #deletes bin folder
+#------------------------build settings--------------------------
+require 'rake-settings.rb'
+
+msbuild_settings = {
+  :properties => {:configuration => :release},
+  :targets => [:clean, :rebuild],
+  :verbosity => :quiet,
+  #:use => :net35  ;uncomment to use .net 3.5 - default is 4.0
+}
+
+#------------------------dependency settings---------------------
+
+#------------------------project settings------------------------
+$basepath = 'http://delicious:8080/svn/habanero/Habanero/branches/v2.6-DotNet2CompactFramework'
+$solution = 'source/Habanero.sln'
+
+#______________________________________________________________________________
+#---------------------------------TASKS----------------------------------------
+
+desc "Runs the build task"
+task :default => [:build]
+
+desc "Builds Habanero, including tests and pushes to local nuget folder"
+task :build => [:clean, :msbuild, :test, :nuget]
+
+desc "Pushes Habanero into the local nuget folder"
+task :nuget => [:publishBaseNugetPackage, :publishConsoleNugetPackage, :publishDBNugetPackage, :publishBONugetPackage ]
+
+#------------------------build habanero --------------------
+
+desc "Cleans the bin folder"
+task :clean do
+	puts cyan("Cleaning bin folder")
 	FileUtils.rm_rf 'bin'
 end
 
-msbuild :msbuild_habanero do |msb| #builds habanero with msbuild
-  msb.targets :Rebuild
-  msb.properties :configuration => :Release
-  msb.path_to_command = "C:/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe"
-  msb.solution = "source/Habanero.sln"
-  msb.verbosity = "quiet"  #verbose
-  #msb.log_level = :verbose
+desc "Builds the solution with msbuild"
+msbuild :msbuild do |msb| 
+	puts cyan("Building #{$solution} with msbuild")
+	msb.update_attributes msbuild_settings
+	msb.solution = $solution
 end
 
-nunit :run_nunit do |nunit|
- nunit.path_to_command = $Nunit_path
- nunit.assemblies 'bin/Habanero.Test.dll','bin\Habanero.Test.Bo.dll','bin\Habanero.Test.Db.dll'
- nunit.options '/xml=nunit-result.xml'
+desc "Runs the tests"
+nunit :test do |nunit|
+	puts cyan("Running tests")
+	nunit.assemblies 'bin\Habanero.Test.dll','bin\Habanero.Test.Bo.dll','bin\Habanero.Test.Db.dll'
+end
+
+desc "Publish the Habanero.Base nuget package"
+pushnugetpackages :publishBaseNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Base.dll"
+  package.Nugetid = "Habanero.Base.v2.6-DotNet2CompactFramework"
+  package.Version = "2.6"
+  package.Description = "Habanero.Base"
+end
+
+desc "Publish the Habanero.BO nuget package"
+pushnugetpackages :publishBONugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.BO.dll"
+  package.Nugetid = "Habanero.BO.v2.6-DotNet2CompactFramework"
+  package.Version = "2.6"
+  package.Description = "Habanero.BO"
 end
 
 
-
-
-# the following are not being used because NCover 1.5.8 (the open source one) doesn't support .NET 4
-
-$Ncover_path = "C:/Program Files (x86)/NCover/NCover.Console.exe"
-$NcoverExp_path = "c:/program files (x86)/ncoverexplorer/ncoverexplorer.console.exe"
-
-ncoverconsole :run_ncover do |ncc| #This runs the ncover and (hopefully nunit) stuff
- ncc.path_to_command = $Ncover_path
- ncc.output :xml => "Coverage.xml" 
- ncc.working_directory = "."
- ncc.cover_assemblies("Habanero.BO")
- #ncc.ignore_assemblies("Habanero.Base.CoverageExcludeAttribute") #this only works with the commercial version of ncover
- 
-#trigger Nunit
-  nunit = NUnitTestRunner.new($Nunit_path)
-# nunit-console-x86.exe is run to prevent the "Profiler connection not established" error from old Ncover versions
- nunit.assemblies 'bin/Habanero.Test.dll','bin\Habanero.Test.Bo.dll' #,'bin\Habanero.Test.Db.dll'
- nunit.options '/xml=nunit-result.xml'
-#nunit.options '/labels'
-#enable this if you want to see pretty test names scrolling in the hudson page while it builds
- 
-#set Nunit as the test runner
- ncc.testrunner = nunit
- ncc.log_level = :verbose
-end
-
-exec :run_ncoverexp do |cmd| # uses a batch file to run ncover because I ran into some very wierd stuff trying to get the parameters to run on an exec task
-	cmd.path_to_command = $NcoverExp_path
-	cmd.parameters "coverage.xml /html /r:4 /p:Habanero.Core"
+desc "Publish the Habanero.DB nuget package"
+pushnugetpackages :publishDBNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.DB.dll"
+  package.Nugetid = "Habanero.DB.v2.6-DotNet2CompactFramework"
+  package.Version = "2.6"
+  package.Description = "Habanero.DB"
 end
---
+++ Rakefile_old.rb
@@ -0,0 +1,61 @@
+require 'rake'
+require 'albacore'
+    
+task :default => [:build_habanero] #this is the starting point for the script
+
+task :build_habanero => [:clean_habanero,:msbuild_habanero,:run_nunit] # a list of tasks spawned off the default task
+
+$Nunit_path = "C:/Program Files (x86)/NUnit 2.5.6/bin/net-2.0/nunit-console-x86.exe"
+
+#build_habanero tasks
+task :clean_habanero do #deletes bin folder
+	FileUtils.rm_rf 'bin'
+end
+
+msbuild :msbuild_habanero do |msb| #builds habanero with msbuild
+  msb.targets :Rebuild
+  msb.properties :configuration => :Release
+  msb.path_to_command = "C:/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe"
+  msb.solution = "source/Habanero.sln"
+  msb.verbosity = "quiet"  #verbose
+  #msb.log_level = :verbose
+end
+
+nunit :run_nunit do |nunit|
+ nunit.path_to_command = $Nunit_path
+ nunit.assemblies 'bin/Habanero.Test.dll','bin\Habanero.Test.Bo.dll','bin\Habanero.Test.Db.dll'
+ nunit.options '/xml=nunit-result.xml'
+end
+
+
+
+
+# the following are not being used because NCover 1.5.8 (the open source one) doesn't support .NET 4
+
+$Ncover_path = "C:/Program Files (x86)/NCover/NCover.Console.exe"
+$NcoverExp_path = "c:/program files (x86)/ncoverexplorer/ncoverexplorer.console.exe"
+
+ncoverconsole :run_ncover do |ncc| #This runs the ncover and (hopefully nunit) stuff
+ ncc.path_to_command = $Ncover_path
+ ncc.output :xml => "Coverage.xml" 
+ ncc.working_directory = "."
+ ncc.cover_assemblies("Habanero.BO")
+ #ncc.ignore_assemblies("Habanero.Base.CoverageExcludeAttribute") #this only works with the commercial version of ncover
+ 
+#trigger Nunit
+  nunit = NUnitTestRunner.new($Nunit_path)
+# nunit-console-x86.exe is run to prevent the "Profiler connection not established" error from old Ncover versions
+ nunit.assemblies 'bin/Habanero.Test.dll','bin\Habanero.Test.Bo.dll' #,'bin\Habanero.Test.Db.dll'
+ nunit.options '/xml=nunit-result.xml'
+#nunit.options '/labels'
+#enable this if you want to see pretty test names scrolling in the hudson page while it builds
+ 
+#set Nunit as the test runner
+ ncc.testrunner = nunit
+ ncc.log_level = :verbose
+end
+
+exec :run_ncoverexp do |cmd| # uses a batch file to run ncover because I ran into some very wierd stuff trying to get the parameters to run on an exec task
+	cmd.path_to_command = $NcoverExp_path
+	cmd.parameters "coverage.xml /html /r:4 /p:Habanero.Core"
+end
