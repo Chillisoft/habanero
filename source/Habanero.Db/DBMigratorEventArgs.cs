b1ccd970a540d21ec090346beaf3d90122d4eda8
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2012-10-03 13:20:03 +0000

    * add event raisers to DBMigrator so that a program can provide user feedback for longer-running migrations

---
+++ source/Habanero.Db/DBMigratorEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Habanero.DB
+{
+    public class DBMigratorEventArgs : EventArgs
+    {
+        public uint StartingStep { get; protected set; }
+        public uint CurrentStep { get; protected set; }
+        public uint TotalSteps { get; protected set; }
+        public decimal PercentageComplete { get; protected set; }
+        public DBMigratorEventArgs(uint startingStep, uint currentStep, uint totalSteps)
+        {
+            this.StartingStep = startingStep;
+            this.CurrentStep = currentStep;
+            this.TotalSteps = totalSteps;
+            if (totalSteps > startingStep)
+                this.PercentageComplete = (100M * (currentStep - startingStep + 1)) / (1 + (decimal)totalSteps - (decimal)startingStep);
+            else
+                this.PercentageComplete = 0;
+        }
+    }
+}
---
+++ source/Habanero.Test.Db/TestDBMigratorEventArgs.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Habanero.Base;
+using Habanero.DB;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Habanero.Test.DB
+{
+    [TestFixture]
+    public class TestDBMigratorEventArgs
+    {
+        [TestCase(1, 1, 2, 50)]
+        [TestCase(1, 1, 4, 25)]
+        [TestCase(1, 2, 10, 20)]
+        public void Constructor_SetsPercentageCompleteToCorrectValue(int start, int step, int total, decimal expectedPercentage)
+        {
+            //---------------Set up test pack-------------------
+            var conn = Substitute.For<IDatabaseConnection>();
+            var migrator = new DBMigrator(conn);
+            
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(migrator);
+
+            //---------------Execute Test ----------------------
+            var e = new DBMigratorEventArgs((uint)start, (uint)step, (uint)total);
+            var propVal = e.PercentageComplete;
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expectedPercentage, propVal);
+        }
+
+        [Test]
+        public void Constructor_SetsTotalStepsToSpecifiedValue()
+        {
+            //---------------Set up test pack-------------------
+            var conn = Substitute.For<IDatabaseConnection>();
+            var migrator = new DBMigrator(conn);
+            var step = (uint)TestUtil.GetRandomInt(1, 50);
+            var totalSteps = (uint)TestUtil.GetRandomInt((int)step, (int)step + 50);
+            
+            //---------------Assert Precondition----------------
+            Assert.AreNotEqual(0, step);
+            Assert.That(totalSteps, Is.GreaterThan(step));
+            Assert.IsNotNull(migrator);
+
+            //---------------Execute Test ----------------------
+            var e = new DBMigratorEventArgs(1, step, totalSteps);
+            var propVal = e.TotalSteps;
+            //---------------Test Result -----------------------
+            Assert.AreEqual(totalSteps, propVal);
+        }
+
+        [Test]
+        public void Constructor_SetsCurrentStepToSpecifiedStep()
+        {
+            //---------------Set up test pack-------------------
+            var conn = Substitute.For<IDatabaseConnection>();
+            var migrator = new DBMigrator(conn);
+            var step = (uint)TestUtil.GetRandomInt(1, 50);
+            var totalSteps = (uint)TestUtil.GetRandomInt((int)step, (int)step + 50);
+            
+            //---------------Assert Precondition----------------
+            Assert.AreNotEqual(0, step);
+            Assert.IsNotNull(migrator);
+
+            //---------------Execute Test ----------------------
+            var e = new DBMigratorEventArgs(1, step, totalSteps);
+            var propVal = e.CurrentStep;
+            //---------------Test Result -----------------------
+            Assert.AreEqual(step, propVal);
+        }
+
+        [Test]
+        public void Constructor_SetsStartingStepToSpecifiedStep()
+        {
+            //---------------Set up test pack-------------------
+            var conn = Substitute.For<IDatabaseConnection>();
+            var migrator = new DBMigrator(conn);
+            var startingStep = (uint)TestUtil.GetRandomInt(1, 50);
+            var step = (uint)TestUtil.GetRandomInt((int)startingStep, (int)startingStep + 50);
+            var totalSteps = (uint)TestUtil.GetRandomInt((int)step, (int)step + 50);
+            
+            //---------------Assert Precondition----------------
+            Assert.AreNotEqual(0, step);
+            Assert.IsNotNull(migrator);
+
+            //---------------Execute Test ----------------------
+            var e = new DBMigratorEventArgs(startingStep, step, totalSteps);
+            var propVal = e.StartingStep;
+            //---------------Test Result -----------------------
+            Assert.AreEqual(startingStep, propVal);
+        }
+
+    }
+}
