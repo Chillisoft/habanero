a5d68a33171715e2899fa6f7b06ddbf918fbbd4f
Andrew Everson <andrew.everson@chillisoft.com>
2013-11-28 11:51:10 +0200

    Change rake to work with Git Submodule and Nuget

--- Rakefile.rb
+++ Rakefile.rb
@@ -8,22 +8,10 @@ require 'albacore'
 # This should be the same for most projects, but if your project is a level
 # deeper in the repo you will need to add another ..
 bs = File.dirname(__FILE__)
-bs = File.join(bs, "../HabaneroCommunity/BuildScripts")
+bs = File.join(bs, "/rake-tasks")
 $buildscriptpath = File.expand_path(bs)
 $:.unshift($buildscriptpath) unless
     $:.include?(bs) || $:.include?($buildscriptpath)
-
-if (bs.index("branches") == nil)	
-	nuget_version = 'Trunk'
-	nuget_version_id = '9.9.999'
-	
-	$nuget_publish_version = nuget_version
-	$nuget_publish_version_id = nuget_version_id
-else
-
-	$nuget_publish_version = 'v3.0'
-	$nuget_publish_version_id = '3.0'
-end		
 
 $binaries_baselocation = "bin"
 $nuget_baselocation = "nugetArtifacts"
@@ -31,7 +19,6 @@ $app_version ='9.9.9.999'
 #------------------------build settings--------------------------
 require 'rake-settings.rb'
 
-puts cyan("Nuget Details #{$nuget_publish_version }, #{$nuget_publish_version_id} ")	
 msbuild_settings = {
   :properties => {:configuration => :debug},
   :targets => [:clean, :rebuild],
@@ -46,49 +33,56 @@ $solution = 'source/Habanero.sln'
 $major_version = ''
 $minor_version = ''
 $patch_version = ''
+$nuget_apikey = ''
+$nuget_sourceurl = ''
+$nuget_publish_version = 'Trunk'
 #______________________________________________________________________________
 #---------------------------------TASKS----------------------------------------
 
 desc "Runs the build task"
-task :default, [:major, :minor, :patch] => [:setupversion, :build]
+task :default, [:major, :minor, :patch] => [:setupvars, :build_test]
 
-desc "Builds Habanero, including tests and pushes to local nuget folder"
-task :build => [:installNugetPackages, :build_only, :test, :nuget]
+desc "Runs the build task and pushes to local"
+task :build_push, [:major, :minor, :patch, :apikey, :sourceurl] => [:setupvars, :build_test, :nugetpush]
+
+desc "Runs the build task and pushes to local"
+task :build_push_nobuild, [:major, :minor, :patch, :apikey, :sourceurl] => [:setupvars, :nugetpush]
 
 desc "Builds Habanero, including tests"
-task :build_test => [:clean, :installNugetPackages, :set_assembly_version, :msbuild, :copy_to_nuget, :test]
+task :build_test => [:build_only, :test]
 
 desc "Builds Habanero"
-task :build_only => [:clean, :set_assembly_version, :msbuild, :copy_to_nuget]
+task :build_only => [:restorepackages,:clean, :set_assembly_version, :msbuild, :copy_to_nuget]
 
 desc "Builds Habanero, including running tests with dotcover then pushes to the local nuget server"
-task :build_with_coverage => [:installNugetPackages, :build_only, :test_with_coverage, :nuget]
+task :build_with_coverage => [:build_only, :test_with_coverage]
 
 desc "Build with sonar (stats build)"
 task :build_with_sonar => [:build_only, :sonar]
 
-desc "Setup Versions"
-task :setupversion,:major ,:minor,:patch do |t, args|
-	puts cyan("Setup Versions")
+desc "Setup Variables"
+task :setupvars,:major ,:minor,:patch, :apikey, :sourceurl do |t, args|
+	puts cyan("Setup Variables")
 	args.with_defaults(:major => "0")
 	args.with_defaults(:minor => "0")
 	args.with_defaults(:patch => "0000")
+	args.with_defaults(:apikey => "")
+	args.with_defaults(:sourceurl => "")
 	$major_version = "#{args[:major]}"
 	$minor_version = "#{args[:minor]}"
 	$patch_version = "#{args[:patch]}"
+	$nuget_apikey = "#{args[:apikey]}"
+	$nuget_sourceurl = "#{args[:sourceurl]}"
 	$app_version = "#{$major_version}.#{$minor_version}.#{$patch_version}.0"
 	puts cyan("Assembly Version #{$app_version}")
+	puts cyan("Nuegt key: #{$nuget_apikey} for: #{$nuget_sourceurl}")
 end
 
-desc "Pushes Habanero into the local nuget folder"
-task :nuget => [:publishBaseNugetPackage, 
-				:publishConsoleNugetPackage, 
-				:publishDBNugetPackage, 
-				:publishBONugetPackage,
-				:publishTestNugetPackage,
-				:publishTestBONugetPackage,
-				:publishTestStructureNugetPackage,
-				:publishTestDBNugetPackage]
+desc "Restore Nuget Packages"
+task :restorepackages do
+	system 'lib\nuget.exe restore source\Habanero.sln'
+end
+
 #------------------------build habanero --------------------
 
 desc "Cleans build folders"
@@ -100,7 +94,6 @@ task :clean do
 	FileUtils.rm_rf $nuget_baselocation	
 	FileSystem.ensure_dir_exists $nuget_baselocation
 end
-
 
 task :set_assembly_version do
 	puts green("Setting Shared AssemblyVersion to: #{$app_version}")
@@ -174,80 +167,9 @@ def copy_nuget_files_to location
 end
 
 task :copy_to_nuget do
-	puts cyan("Copying files to the nuget folder")
-	
+	puts cyan("Copying files to the nuget folder")	
 	copy_nuget_files_to $nuget_baselocation
 end
-
-desc "Install nuget packages"
-getnugetpackages :installNugetPackages do |ip|
-    ip.package_names = ["nunit.trunk"]
-end
-
-desc "Publish the Habanero.Base nuget package"
-pushnugetpackages :publishBaseNugetPackage do |package|
-  package.InputFileWithPath = "bin/Habanero.Base.dll"
-  package.Nugetid = "Habanero.Base.#{$nuget_publish_version}"
-  package.Version = $nuget_publish_version_id
-  package.Description = "Habanero.Base"
-end
-
-desc "Publish the Habanero.BO nuget package"
-pushnugetpackages :publishBONugetPackage do |package|
-  package.InputFileWithPath = "bin/Habanero.BO.dll"
-  package.Nugetid = "Habanero.BO.#{$nuget_publish_version}"
-  package.Version = $nuget_publish_version_id
-  package.Description = "Habanero.BO"
-end
-
-desc "Publish the Habanero.Console nuget package"
-pushnugetpackages :publishConsoleNugetPackage do |package|
-  package.InputFileWithPath = "bin/Habanero.Console.dll"
-  package.Nugetid = "Habanero.Console.#{$nuget_publish_version}"
-  package.Version = $nuget_publish_version_id
-  package.Description = "Habanero.Console"
-end
-
-desc "Publish the Habanero.DB nuget package"
-pushnugetpackages :publishDBNugetPackage do |package|
-  package.InputFileWithPath = "bin/Habanero.DB.dll"
-  package.Nugetid = "Habanero.DB.#{$nuget_publish_version}"
-  package.Version = $nuget_publish_version_id
-  package.Description = "Habanero.DB"
-end
-
-desc "Publish the Habanero.Test nuget package"
-pushnugetpackages :publishTestNugetPackage do |package|
-  package.InputFileWithPath = "bin/Habanero.Test.dll"
-  package.Nugetid = "Habanero.Test.#{$nuget_publish_version}"
-  package.Version = $nuget_publish_version_id
-  package.Description = "Habanero.Test"
-end
-
-desc "Publish the Habanero.Test.BO nuget package"
-pushnugetpackages :publishTestBONugetPackage do |package|
-  package.InputFileWithPath = "bin/Habanero.Test.BO.dll"
-  package.Nugetid = "Habanero.Test.BO.#{$nuget_publish_version}"
-  package.Version = $nuget_publish_version_id
-  package.Description = "Habanero.Test.BO"
-end
-
-desc "Publish the Habanero.Test.Structure nuget package"
-pushnugetpackages :publishTestStructureNugetPackage do |package|
-  package.InputFileWithPath = "bin/Habanero.Test.Structure.dll"
-  package.Nugetid = "Habanero.Test.Structure.#{$nuget_publish_version}"
-  package.Version = $nuget_publish_version_id
-  package.Description = "Habanero.Test.Structure"
-end
-
-desc "Publish the Habanero.Test.DB nuget package"
-pushnugetpackages :publishTestDBNugetPackage do |package|
-  package.InputFileWithPath = "bin/Habanero.Test.DB.dll"
-  package.Nugetid = "Habanero.Test.DB.#{$nuget_publish_version}"
-  package.Version = $nuget_publish_version_id
-  package.Description = "Habanero.Test.DB"
-end
-
 
 #------------------------------------------Build Stats------------------------------------------------------------
 
@@ -257,4 +179,98 @@ exec :sonar do |cmd|
   cmd.command = "cmd.exe"
   cmd.parameters = "/c #{$sonar_runner_path}"
 end
+
+#------------------------------------------Single Dlls, Nuget Package Push For Internal Use------------------------------------------------------------
+desc "Pushes Habanero into the given nuget folder"
+task :nugetpush => [:publishBaseNugetPackage, 
+				:publishConsoleNugetPackage, 
+				:publishDBNugetPackage, 
+				:publishBONugetPackage,
+				:publishTestNugetPackage,
+				:publishTestBONugetPackage,
+				:publishTestStructureNugetPackage,
+				:publishTestDBNugetPackage]
+
+desc "Publish the Habanero.Base nuget package"
+pushnugetpackagesonline :publishBaseNugetPackage do |package|
+puts cyan("Habanero.Base.#{$nuget_publish_version} ,Version: #{$app_version} ,ApiKey: #{$nuget_apikey},Url: #{$nuget_sourceurl}  ")
+  package.InputFileWithPath = "bin/Habanero.Base.dll"
+  package.Nugetid = "Habanero.Base.#{$nuget_publish_version}"
+  package.Version = $app_version
+  package.Description = "Habanero.Base"
+  package.ApiKey = "#{$nuget_apikey}"
+  package.SourceUrl = "#{$nuget_sourceurl}"
+end
+
+desc "Publish the Habanero.BO nuget package"
+pushnugetpackagesonline :publishBONugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.BO.dll"
+  package.Nugetid = "Habanero.BO.#{$nuget_publish_version}"
+  package.Version = $app_version
+  package.Description = "Habanero.BO"
+  package.ApiKey = "#{$nuget_apikey}"
+  package.SourceUrl = "#{$nuget_sourceurl}"
+end
+
+desc "Publish the Habanero.Console nuget package"
+pushnugetpackagesonline :publishConsoleNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Console.dll"
+  package.Nugetid = "Habanero.Console.#{$nuget_publish_version}"
+  package.Version = $app_version
+  package.Description = "Habanero.Console"
+  package.ApiKey = "#{$nuget_apikey}"
+  package.SourceUrl = "#{$nuget_sourceurl}"
+end
+
+desc "Publish the Habanero.DB nuget package"
+pushnugetpackagesonline :publishDBNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.DB.dll"
+  package.Nugetid = "Habanero.DB.#{$nuget_publish_version}"
+  package.Version = $app_version
+  package.Description = "Habanero.DB"
+  package.ApiKey = "#{$nuget_apikey}"
+  package.SourceUrl = "#{$nuget_sourceurl}"
+end
+
+desc "Publish the Habanero.Test nuget package"
+pushnugetpackagesonline :publishTestNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Test.dll"
+  package.Nugetid = "Habanero.Test.#{$nuget_publish_version}"
+  package.Version = $app_version
+  package.Description = "Habanero.Test"
+  package.ApiKey = "#{$nuget_apikey}"
+  package.SourceUrl = "#{$nuget_sourceurl}"
+end
+
+desc "Publish the Habanero.Test.BO nuget package"
+pushnugetpackagesonline :publishTestBONugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Test.BO.dll"
+  package.Nugetid = "Habanero.Test.BO.#{$nuget_publish_version}"
+  package.Version = $app_version
+  package.Description = "Habanero.Test.BO"
+  package.ApiKey = "#{$nuget_apikey}"
+  package.SourceUrl = "#{$nuget_sourceurl}"
+end
+
+desc "Publish the Habanero.Test.Structure nuget package"
+pushnugetpackagesonline :publishTestStructureNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Test.Structure.dll"
+  package.Nugetid = "Habanero.Test.Structure.#{$nuget_publish_version}"
+  package.Version = $app_version
+  package.Description = "Habanero.Test.Structure"
+  package.ApiKey = "#{$nuget_apikey}"
+  package.SourceUrl = "#{$nuget_sourceurl}"
+end
+
+desc "Publish the Habanero.Test.DB nuget package"
+pushnugetpackagesonline :publishTestDBNugetPackage do |package|
+  package.InputFileWithPath = "bin/Habanero.Test.DB.dll"
+  package.Nugetid = "Habanero.Test.DB.#{$nuget_publish_version}"
+  package.Version = $app_version
+  package.Description = "Habanero.Test.DB"
+  package.ApiKey = "#{$nuget_apikey}"
+  package.SourceUrl = "#{$nuget_sourceurl}"
+end
+
+
 
