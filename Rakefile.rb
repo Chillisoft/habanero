edf189514ce627c574e1d1784757620169c672b4
Kevin Bosman <kevin.bosman@chillisoft.co.za>
2015-01-09 11:45:25 +0200

    Updated Rakefile, rake-tasks and sonar config

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,202 +1,178 @@
-require 'rake'
-require 'albacore'
+require "./rake-tasks/init"
 
-#______________________________________________________________________________
-#---------------------------------SETTINGS-------------------------------------
-
-# set up the build script folder so we can pull in shared rake scripts.
-# This should be the same for most projects, but if your project is a level
-# deeper in the repo you will need to add another ..
-bs = File.dirname(__FILE__)
-bs = File.join(bs, "/rake-tasks")
-$buildscriptpath = File.expand_path(bs)
-$:.unshift($buildscriptpath) unless
-    $:.include?(bs) || $:.include?($buildscriptpath)
+#-----------------------------------SETTINGS-----------------------------------
 
 $binaries_baselocation = "bin"
-$nuget_baselocation = "nugetArtifacts"
-$app_version ='9.9.9.999'
-#------------------------build settings--------------------------
-require 'rake-settings.rb'
+$nuget_baselocation = "nugetArtifacts/"
+$app_version = "9.9.9.999"
+
+#--------------------------------build settings--------------------------------
+
+$build_configuration = "Debug"
 
 msbuild_settings = {
-  :properties => {:configuration => :debug},
+  :properties => {:configuration => $build_configuration},
   :targets => [:clean, :rebuild],
-  :verbosity => :quiet,
+  :verbosity => :minimal,
   #:use => :net35  ;uncomment to use .net 3.5 - default is 4.0
 }
 
-#------------------------dependency settings---------------------
+#-------------------------------project settings-------------------------------
 
-#------------------------project settings------------------------
-$solution = 'source/Habanero.sln'
-$solutionNuget = '"source/Habanero.sln"'
-$major_version = ''
-$minor_version = ''
-$patch_version = ''
-$nuget_apikey = ''
-$nuget_sourceurl = ''
-$nuget_publish_version = 'Trunk'
+$solution = "source/Habanero.sln"
+$major_version = ""
+$minor_version = ""
+$patch_version = ""
+$nuget_apikey = ""
+$nuget_sourceurl = ""
+$nuget_publish_version = "Trunk"
+
 #______________________________________________________________________________
-#---------------------------------TASKS----------------------------------------
+#-------------------------------------TASKS------------------------------------
 
 desc "Runs the build task"
-task :default, [:major, :minor, :patch] => [:updatesubmodules, :setupvars, :build_test]
+task :default, [:major, :minor, :patch] => [:setupvars, :build_test]
 
 desc "Runs the build task and pushes to local"
-task :build_push, [:major, :minor, :patch, :apikey, :sourceurl] => [:updatesubmodules, :setupvars, :build_test, :nugetpush]
+task :build_push, [:major, :minor, :patch, :apikey, :sourceurl] => [:setupvars, :build_test, :nugetpush]
 
-desc "Runs the build task and pushes to local"
+desc "Pushes NuGet packages to local"
 task :build_push_nobuild, [:major, :minor, :patch, :apikey, :sourceurl] => [:setupvars, :nugetpush]
 
 desc "Builds Habanero, including tests"
 task :build_test => [:build_only, :test]
 
 desc "Builds Habanero"
-task :build_only, [:major, :minor, :patch, :apikey, :sourceurl] => [:updatesubmodules,:restorepackages,:clean, :set_assembly_version, :msbuild, :copy_to_nuget]
+task :build_only, [:major, :minor, :patch, :apikey, :sourceurl] => [:update_packages, :clean, :set_assembly_version, :msbuild, :copy_to_nuget]
 
 desc "Builds Habanero, including running tests with dotcover then pushes to the local nuget server"
 task :build_with_coverage, [:major, :minor, :patch, :apikey, :sourceurl] => [:build_only, :test_with_coverage]
 
 desc "Build with sonar (stats build)"
-task :build_with_sonar, [:major, :minor, :patch, :apikey, :sourceurl] => [:build_only, :sonar]
+task :build_with_sonar, [:major, :minor, :patch, :apikey, :sourceurl] => [:build_with_coverage, :sonar]
 
-desc "Setup Variables"
-task :setupvars,:major ,:minor,:patch, :apikey, :sourceurl do |t, args|
-	puts cyan("Setup Variables")
-	args.with_defaults(:major => "0")
-	args.with_defaults(:minor => "0")
-	args.with_defaults(:patch => "0000")
-	args.with_defaults(:apikey => "")
-	args.with_defaults(:sourceurl => "")
-	$major_version = "#{args[:major]}"
-	$minor_version = "#{args[:minor]}"
-	$patch_version = "#{args[:patch]}"
-	$nuget_apikey = "#{args[:apikey]}"
-	$nuget_sourceurl = "#{args[:sourceurl]}"
-	$app_version = "#{$major_version}.#{$minor_version}.#{$patch_version}.0"
-	puts cyan("Assembly Version #{$app_version}")
-	puts cyan("Nuget key: #{$nuget_apikey} for: #{$nuget_sourceurl}")
+#--------------------------------Update packages-------------------------------
+
+desc "Updates packages with nuget"
+updatenugetpackages :update_packages do |nuget|
+    puts cyan("Updating nuget packages")
+    nuget.solution = $solution
 end
 
-desc "Restore Nuget Packages"
-task :restorepackages do
-	system 'lib\nuget.exe restore #{$solutionNuget}'
+#-------------------------------Prepare For Build------------------------------
+
+task :setupvars, :major, :minor, :patch, :apikey, :sourceurl do |t, args|
+    puts cyan("Setup Variables")
+    args.with_defaults(:major => "0")
+    args.with_defaults(:minor => "0")
+    args.with_defaults(:patch => "0000")
+    args.with_defaults(:apikey => "")
+    args.with_defaults(:sourceurl => "")
+    $major_version = "#{args[:major]}"
+    $minor_version = "#{args[:minor]}"
+    $patch_version = "#{args[:patch]}"
+    $nuget_apikey = "#{args[:apikey]}"
+    $nuget_sourceurl = "#{args[:sourceurl]}"
+    $app_version = "#{$major_version}.#{$minor_version}.#{$patch_version}.0"
+    puts cyan("Assembly Version #{$app_version}")
+    puts cyan("Nuget key: #{$nuget_apikey} for: #{$nuget_sourceurl}")
 end
 
-desc "Update Submodules"
-task :updatesubmodules do
-	puts cyan("Updating Git Submodules")
-	system 'git submodule foreach git checkout master'
-	system 'git submodule foreach git pull'
-end
-#------------------------build habanero --------------------
-
-desc "Cleans build folders"
+desc "Cleans the bin folder and all project bin folders"
 task :clean do
-	puts cyan("Cleaning build folders")
-	FileUtils.rm_rf $binaries_baselocation
-	FileSystem.ensure_dir_exists "#{$binaries_baselocation}/Debug"
-	FileSystem.ensure_dir_exists "#{$binaries_baselocation}/Release"
-	FileUtils.rm_rf $nuget_baselocation	
-	FileSystem.ensure_dir_exists $nuget_baselocation
+    puts cyan("Cleaning all bin and test folders")
+    clean_build_output_directories
+    FileUtils.rm_rf $binaries_baselocation
+    FileUtils.rm_rf $nuget_baselocation
+end
+
+def clean_build_output_directories
+    bin_dirs = Dir.glob("**/bin").select {|f| (File.directory? f) && !(f.match(/\/node_modules\//) || f.match(/\/packages\//) || f.match(/\/obj\//) || f.match(/\/rake-tasks\//))}
+    for project_output in bin_dirs
+        FileUtils.rm_rf project_output
+    end
 end
 
 task :set_assembly_version do
-	puts green("Setting Shared AssemblyVersion to: #{$app_version}")
-	file_path = "source/Common/AssemblyInfoShared.cs"
-	outdata = File.open(file_path).read.gsub(/"9.9.9.999"/, "\"#{$app_version}\"")
-	File.open(file_path, 'w') do |out|
-		out << outdata
-	end	
+    puts green("Setting Shared AssemblyVersion to: #{$app_version}")
+    file_path = "source/Common/AssemblyInfoShared.cs"
+    outdata = File.open(file_path).read.gsub(/"9.9.9.999"/, "\"#{$app_version}\"")
+    File.open(file_path, "w") do |out|
+        out << outdata
+    end
 end
 
+#--------------------------------Build Solution--------------------------------
+
 desc "Builds the solution with msbuild"
-msbuild :msbuild do |msb| 
-	puts cyan("Building #{$solution} with msbuild")
-	msb.update_attributes msbuild_settings
-	msb.solution = $solution
+msbuild :msbuild do |msb|
+    puts cyan("Building #{$solution} with msbuild")
+    msb.update_attributes msbuild_settings
+    msb.solution = $solution
 end
+
+#-----------------------------------Run Tests----------------------------------
 
 desc "Runs the tests"
 nunit :test do |nunit|
-	puts cyan("Running tests")
-	nunit.assemblies testassemblies
-end
-
-def testassemblies
-	['bin\Habanero.Test.dll',
-	'bin\Habanero.Test.Bo.dll',
-	'bin\Habanero.Test.Db.dll']
+    puts cyan("Running tests")
+    nunit.assemblies testassemblies
 end
 
 desc "Runs the tests with dotcover"
 dotcover :test_with_coverage do |dc|
-	puts cyan("Running tests with dotcover")
-	dc.assemblies testassemblies
-    dc.filters '+:module=*;class=*;function=*'
+    puts cyan("Running tests with dotcover")
+    dc.assemblies testassemblies
+    dc.filters "+:module=*;class=*;function=*"
 end
 
-def nugetassemblieswithpdb
-	["#{$binaries_baselocation}/#{$build_configuration}\Habanero.Base.dll",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.BO.dll",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Console.dll",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Test.dll",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Test.BO.dll",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Test.Structure.dll",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Test.DB.dll",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.DB.dll",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Base.pdb",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.BO.pdb",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Console.pdb",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Test.pdb",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Test.BO.pdb",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Test.Structure.pdb",
-	"#{$binaries_baselocation}/#{$build_configuration}\Habanero.Test.DB.pdb",]
+def testassemblies
+    [
+        "bin/Habanero.Test.dll",
+        "bin/Habanero.Test.BO.dll",
+        "bin/Habanero.Test.DB.dll",
+        "bin/Habanero.Test.Structure.dll",
+    ]
+end
+
+#----------------------------------Copy Tasks----------------------------------
+
+task :copy_to_nuget do
+    puts cyan("Copying files to the nuget folder")
+    copy_nuget_files_to $nuget_baselocation
 end
 
 def copy_nuget_files_to location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Base.dll", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.BO.dll", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Console.dll", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Test.dll", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Test.BO.dll", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Test.Structure.dll", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Test.DB.dll", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.DB.dll", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Base.pdb", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.BO.pdb", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Console.pdb", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Test.pdb", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Test.BO.pdb", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Test.Structure.pdb", location
-	FileUtils.cp "#{$binaries_baselocation}/#{$build_configuration}/Habanero.Test.DB.pdb", location
+    FileSystem.ensure_dir_exists "#{location}/"
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Base.dll", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.BO.dll", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Console.dll", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.DB.dll", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Test.dll", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Test.BO.dll", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Test.Structure.dll", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Test.DB.dll", location
+
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Base.pdb", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.BO.pdb", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Console.pdb", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.DB.pdb", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Test.pdb", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Test.BO.pdb", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Test.Structure.pdb", location
+    FileUtils.cp "#{$binaries_baselocation}/Habanero.Test.DB.pdb", location
 end
 
-task :copy_to_nuget do
-	puts cyan("Copying files to the nuget folder")	
-	copy_nuget_files_to $nuget_baselocation
-end
-
-#------------------------------------------Build Stats------------------------------------------------------------
-
-desc "Runs sonar"
-exec :sonar do |cmd|
-  puts cyan("Running Sonar")
-  cmd.command = "cmd.exe"
-  cmd.parameters = "/c #{$sonar_runner_path}"
-end
-
-#------------------------------------------Single Dlls, Nuget Package Push For Internal Use------------------------------------------------------------
+#---------------Single Dlls, Nuget Package Push For Internal Use---------------
 desc "Pushes Habanero into the given nuget folder"
-task :nugetpush => [:publishBaseNugetPackage, 
-				:publishConsoleNugetPackage, 
-				:publishDBNugetPackage, 
-				:publishBONugetPackage,
-				:publishTestNugetPackage,
-				:publishTestBONugetPackage,
-				:publishTestStructureNugetPackage,
-				:publishTestDBNugetPackage]
+task :nugetpush => [:publishBaseNugetPackage,
+                :publishConsoleNugetPackage,
+                :publishDBNugetPackage,
+                :publishBONugetPackage,
+                :publishTestNugetPackage,
+                :publishTestBONugetPackage,
+                :publishTestStructureNugetPackage,
+                :publishTestDBNugetPackage]
 
 desc "Publish the Habanero.Base nuget package"
 pushnugetpackagesonline :publishBaseNugetPackage do |package|
@@ -278,6 +254,3 @@ pushnugetpackagesonline :publishTestDBNugetPackage do |package|
   package.ApiKey = "#{$nuget_apikey}"
   package.SourceUrl = "#{$nuget_sourceurl}"
 end
-
-
-
