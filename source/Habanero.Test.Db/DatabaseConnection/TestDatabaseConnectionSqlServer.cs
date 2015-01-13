270baff08965f2e5a8e2bd8b53597d4ec5a8f922
Kevin Bosman <kevin.bosman@chillisoft.co.za>
2015-01-13 16:03:04 +0200

    Corrected more test database names

--- source/Habanero.Test.Db/DatabaseConnection/TestDatabaseConnectionSqlServer.cs
+++ source/Habanero.Test.Db/DatabaseConnection/TestDatabaseConnectionSqlServer.cs
@@ -46,7 +46,7 @@ namespace Habanero.Test.DB
             IDatabaseConnection originalConnection = DatabaseConnection.CurrentConnection;
             DatabaseConfig databaseConfig = new DatabaseConfig("SqlServer", "localhost", "habanero_test_branch_2_6", "sa", "sa", null);
             DatabaseConnection.CurrentConnection = databaseConfig.GetDatabaseConnection();
-            //DatabaseConnection.CurrentConnection = new DatabaseConnectionSqlServer("System.Data", "System.Data.SqlClient.SqlConnection","server=localhost;database=habanero_test_trunk;user=sa;password=sa");
+            //DatabaseConnection.CurrentConnection = new DatabaseConnectionSqlServer("System.Data", "System.Data.SqlClient.SqlConnection","server=localhost;database=habanero_test_branch_2_6;user=sa;password=sa");
             const string sql = "Select FirstName + ', ' + Surname from tbPersonTable";
             SqlStatement sqlStatement = new SqlStatement(DatabaseConnection.CurrentConnection, sql);
 
